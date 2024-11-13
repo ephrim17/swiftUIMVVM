ae908f2ae9e3d4aaf31a00b3b7eda3fd2a90b33e
Ephrim <mirhpe17@gmail.com>
2024-11-13 14:54:45 +0530

    Initial Commit

---
+++ SwiftMVVM/SwiftMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMVVMApp.swift
+//  SwiftMVVM
+//
+//  Created by Ephrim Daniel on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
